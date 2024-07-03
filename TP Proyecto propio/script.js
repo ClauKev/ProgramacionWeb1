ac2c5dc753b981338acd6ad13c1c2c8a8b2c4e9e
ClauKev <kevinxvdp@gmail.com>
2024-07-03 19:03:00 -0300

    Agrego de Payment

--- TP Proyecto propio/script.js
+++ TP Proyecto propio/script.js
@@ -114,3 +114,96 @@ function ValidarEmail(){
     document.getElementById("formulario").submit();
   }
 }
+
+// Sistema de pago
+
+function ValidarPayment(){
+  var nombre = document.getElementById("nombre").value;
+  var direccion = document.getElementById("direccion").value;
+  var email = document.getElementById("email").value;
+  var ciudad = document.getElementById("ciudad").value;
+  var codigoPostal = document.getElementById("Codigop").value;
+  var titular = document.getElementById("titular").value;
+  var numeroTarjeta = document.getElementById("Nt").value;
+  var fechaCaducidad = document.getElementById("FdC").value;
+  var codigoSeguridad = document.getElementById("CdS").value;
+
+  var validator = true;
+
+  // Nombre
+  if (nombre == "") {
+      document.getElementById("ErrorName").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorName").style.display = "none";
+  }
+
+  // Dirección
+  if (direccion == "") {
+      document.getElementById("ErrorDireccion").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorDireccion").style.display = "none";
+  }
+
+  // Email
+  if (email == "") {
+      document.getElementById("ErrorEmail").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorEmail").style.display = "none";
+  }
+
+  // Ciudad
+  if (ciudad == "") {
+      document.getElementById("ErrorCiudad").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorCiudad").style.display = "none";
+  }
+
+  // Código Postal
+  if (codigoPostal.length != 4) {
+      document.getElementById("ErrorCodigop").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorCodigop").style.display = "none";
+  }
+
+  // Titular de la tarjeta
+  if (titular == "") {
+      document.getElementById("ErrorTitular").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorTitular").style.display = "none";
+  }
+
+  // Número de tarjeta
+  if (numeroTarjeta.length != 16) {
+      document.getElementById("ErrorNt").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorNt").style.display = "none";
+  }
+
+  // Fecha de caducidad
+  if (fechaCaducidad == "") {
+      document.getElementById("ErrorFdC").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorFdC").style.display = "none";
+  }
+
+  // Código de seguridad
+  if (codigoSeguridad.length != 3) {
+      document.getElementById("ErrorCdS").style.display = "block";
+      validator = false;
+  } else {
+      document.getElementById("ErrorCdS").style.display = "none";
+  }
+
+  if (validator == true) {
+      document.getElementById("formulario").submit();
+      alert("Esta es una cuenta de administrador");
+  }
+}
